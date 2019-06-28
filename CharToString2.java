8816364cc347db172ff54163b3a62e9f8d0fe743
udamadu11 <madu12dara@gmail.com>
2019-06-28 22:01:18 +0530

    CharToString2

---
+++ CharToString2.java
@@ -0,0 +1,7 @@
+public class CharToString2{
+    public static void main(String[] args){
+        char ch = 'a';
+        String str = String.valueOf(ch);
+        System.out.println(str);
+    }
+}
